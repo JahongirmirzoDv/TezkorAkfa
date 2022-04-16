a1da3a9454e3d10459711ff0ca8490db2213eebf
JahongirmirzoDv <jahongirmirzotolqinov6@gmail.com>
2022-04-16 20:51:59 +0500

    Commit

--- app/src/main/java/uz/algorithmgateway/tezkorakfa/ui/splash/SplashActivity.kt
+++ app/src/main/java/uz/algorithmgateway/tezkorakfa/ui/splash/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import uz.algorithmgateway.tezkorakfa.R
+import uz.algorithmgateway.tezkorakfa.ui.login.LoginActivity
 
 /**
  * Created by Abrorjon Berdiyorov on 04.03.2022
@@ -20,7 +21,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            setResult(Activity.RESULT_OK, Intent())
+            setResult(Activity.RESULT_OK, Intent(this,LoginActivity::class.java))
             finish()
         }, 3000)
     }
