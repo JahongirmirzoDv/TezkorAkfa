62dc71e3c7a4132542bfda1626ec81446eab33cd
JahongirmirzoDv <jahongirmirzotolqinov6@gmail.com>
2022-05-10 11:38:11 +0500

    commit:

--- app/src/main/java/uz/algorithmgateway/tezkorakfa/measurer/ui/customer_photo/CusterSendResource.kt
+++
@@ -1,9 +0,0 @@
-package uz.algorithmgateway.tezkorakfa.measurer.ui.customer_photo
-
-sealed class CusterSendResource {
-
-    object Loading : CusterSendResource()
-    data class Error(val message: String) : CusterSendResource()
-    data class Succful(val succful: Boolean) : CusterSendResource()
-
-}
